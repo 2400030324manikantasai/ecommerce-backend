eddbcbee5e8ed0b6b57e24b7a68a798faf878c38
2400030324manikantasai <2400030324@kluniversity.in>
2026-03-26 01:22:46 +0530

    Update UserService.java

--- src/main/java/com/klu/service/UserService.java
+++ src/main/java/com/klu/service/UserService.java
@@ -5,26 +5,12 @@ import com.klu.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public User login(String email, String password) {
+    User user = userRepository.findByEmail(email);
 
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    // Signup
-    public User registerUser(User user) {
-        return userRepository.save(user);
-    }
-
-    // Login
-    public User login(String email, String password) {
-        User user = userRepository.findByEmail(email);
-
-        if(user != null && user.getPassword().equals(password)) {
-            return user;
-        }
-        return null;
+    if (user != null && user.getPassword().equals(password)) {
+        return user;
+    } else {
+        throw new RuntimeException("Invalid credentials");
     }
 }
